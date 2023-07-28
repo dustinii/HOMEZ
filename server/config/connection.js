bf97996025b94269482f8efdddc629da50874124
sammyrey6 <121529677+sammyrey6@users.noreply.github.com>
2023-07-27 20:57:49 -0700

    Added a clean database for the seeders (not done with the seeds)

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/homez');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/homez_db');
+
 module.exports = mongoose.connection;
