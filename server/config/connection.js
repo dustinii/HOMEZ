225bc93b5e09e75e4859a3096c4462b0e1df80fd
sammyrey6 <121529677+sammyrey6@users.noreply.github.com>
2023-07-27 20:59:48 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/homez_db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/homez_db');
 
 module.exports = mongoose.connection;
