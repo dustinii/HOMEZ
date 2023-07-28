3962d73ccb9ec1eec63e0a4d98637c8d597fc800
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-07-28 00:10:37 -0700

    Update navbar styling

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ function App() {
       
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
         </nav>
 
         <Routes>
