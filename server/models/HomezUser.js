6bb42780aec3cd91263a5544969d523713def223
hokin2023 <hokin990@gmail.com>
2023-07-28 15:24:50 -0700

    added Homez and rider models

---
+++ server/models/HomezUser.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+const HomezUserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+HomezUserSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+HomezUserSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+
+const HomezUser = model('HomezUser', HomezUserSchema);
+
+module.exports = HomezUser;
---
+++ server/models/Rider.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+
+const riderSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+   
+   
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash rider password
+riderSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+riderSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+
+const Rider = model('Rider', riderSchema);
+
+module.exports = Rider;
