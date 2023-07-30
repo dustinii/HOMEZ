a23b5703223e5a0dc0e7c2193896a6f86bdb2886
Paul Koehler <paul.koehler@gmail.com>
2023-07-30 13:18:03 -0700

    server backend template code added, README modified

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,5 @@
 const { signToken } = require('../utils/auth');
 // going to need this for the login and signup mutations.
+const { Homie, Ride, User } = require('../models');
+
+module.exports = resolvers;
