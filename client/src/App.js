c9e3ae8c5028c474993e04ce5c39ce9dfafa8305
Joceliyn <jocevaldez98@gmail.com>
2023-07-30 20:33:50 -0700

    created sign up and login in pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Footer from "./components/Footer";
+// import Image from "./components/Image";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
@@ -22,7 +23,7 @@ function App() {
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
         </nav>
-
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
