dafd50d228ed76b0f30ca04718dfd3c122ed3533
Ortizzz-j <124862900+Ortizzz-j@users.noreply.github.com>
2023-07-31 20:44:44 -0700

    added some things into the app.js in client line 18-37

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink  } from "@apollo/client";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Ride from "./pages/Ride";
@@ -15,8 +15,24 @@ import "bootstrap/dist/js/bootstrap.bundle";
 
 import "./App.css";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: "/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
