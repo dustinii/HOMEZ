975cb5db00fad1809dd1e23594e9dfe2085f7920
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-08-03 19:26:07 -0700

    Updates to signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,48 +1,46 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
-import { Button, Form, Col, Row, Alert, Container } from 'react-bootstrap';
+import { Button, Form, Col, Alert, Container } from 'react-bootstrap';
 import { ADD_HOMEZ } from '../utils/mutations';
 import Auth from '../utils/auth'
-// import HomezNavBar
 import HomezNavbar from '../components/Navbar';
-
 
 const SignupHomez = () => {
 
-  const [userFormData, setUserFormData] = useState(({ username: '', email: '', password: ''}))
+  const [userFormData, setUserFormData] = useState(({ username: '', email: '', password: '', role: '' }))
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   const [addHomezUser, { error }] = useMutation(ADD_HOMEZ);
-  
+
   useEffect(() => {
     if (error) {
       setShowAlert(true);
-    } else{
+    } else {
       setShowAlert(false)
     }
   }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({...userFormData, [name] : value})
+    setUserFormData({ ...userFormData, [name]: value })
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
     const form = event.currentTarget;
-    if(form.checkValidity() === false) {
+    if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
 
-    try{
+    try {
       const { data } = await addHomezUser({
         variables: { ...userFormData },
       });
       console.log(data)
       Auth.login(data.addHomezUser.token)
-    } catch(err){
+    } catch (err) {
       console.log(error)
     }
 
@@ -50,54 +48,74 @@ const SignupHomez = () => {
       username: '',
       email: '',
       password: '',
+      role: '',
     });
   };
 
+  return (
+    <>
+      <HomezNavbar />
+      <Container className="d-flex justify-content-center">
+        <Col md={6}>
+          <h1 className='text-center' style={{ marginTop: '50px' }}>Register with HOMEZ:</h1>
+          <h3 className='text-center'>For Worry-Free Nights Out</h3>
 
+          <Form noValidate validated={validated} onSubmit={handleFormSubmit} style={{ marginBottom: '50px' }}>
+            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>Something went wrong with your signup</Alert>
 
-    return (
-      <>
-          <HomezNavbar />
-          <h1 className='signupTitle'>Register with HOMEZ: For Worry-Free Nights Out</h1>
-
-        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-          <div className='signupForm'>
-          <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>Something went wrong with your signup</Alert>
-
-            <Form.Group as={Col} controlId="formGridFirstName">
+            <Form.Group controlId="formGridFirstName" style={{ marginBottom: '20px' }}>
               <Form.Label>username</Form.Label>
-              <Form.Control type="text" placeholder="Enter first name" name="username" onChange={handleInputChange} value={userFormData.username} required/>
+              <Form.Control type="text" placeholder="Enter first name" name="username" onChange={handleInputChange} value={userFormData.username} required />
               <Form.Control.Feedback type="invalid">username is required!</Form.Control.Feedback>
             </Form.Group>
- 
 
-                <Form.Group as={Col} controlId="formGridFirstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter first name" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridLastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter last name" />
-          </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Group controlId="formGridEmail" style={{ marginBottom: '20px' }}>
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email"  name="email" onChange={handleInputChange} value={userFormData.email} required/>
+              <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleInputChange} value={userFormData.email} required />
               <Form.Control.Feedback type="invalid">email is required!</Form.Control.Feedback>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Group controlId="formGridPassword" style={{ marginBottom: '20px' }}>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter a password" name="password" onChange={handleInputChange} value={userFormData.password} required/>
+              <Form.Control type="password" placeholder="Enter a password" name="password" onChange={handleInputChange} value={userFormData.password} required />
               <Form.Control.Feedback type="invalid">password is required!</Form.Control.Feedback>
             </Form.Group>
-    
-          <Button disabled={!(userFormData.username && userFormData.email && userFormData.password)} type='submit' className="SignupBtn" variant="outline-success">Submit Homez!</Button>
-          </div>
-        </Form>
-        </>
-      );
-} 
+
+            <Form.Group style={{ marginBottom: '20px' }}>
+              <Form.Label>Are you a rider or a driver?</Form.Label>
+              <div className="mb-3 d-flex justify-content-center align-items-center">
+                <Form.Check
+                  className="mx-2"
+                  inline
+                  type='radio'
+                  id='rider'
+                  label='Rider'
+                  name='role'
+                  value='Rider'
+                  onChange={handleInputChange}
+                />
+
+                <Form.Check
+                  className="mx-2"
+                  inline
+                  type='radio'
+                  id='driver'
+                  label='Driver'
+                  name='role'
+                  value='Driver'
+                  onChange={handleInputChange}
+                />
+              </div>
+            </Form.Group>
+
+            <div className="d-flex justify-content-center" style={{ marginBottom: '50px' }}>
+              <Button disabled={!(userFormData.username && userFormData.email && userFormData.password)} type='submit' className="SignupBtn" variant="outline-success">Submit Homez!</Button>
+            </div>
+          </Form>
+        </Col>
+      </Container>
+    </>
+  );
+}
 
 export default SignupHomez;
