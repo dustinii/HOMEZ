20b78c1c24b9e8f9755505b499b2fa47885025e7
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-08-03 20:08:27 -0700

    Update styling for login page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,19 +3,37 @@ import { Link } from "react-router-dom";
 import HomezNavbar from "../components/Navbar";
 import laSkyline from "../images/laSkyline.jpg";
 import behindWheel from "../images/behindWheel.jpg";
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 function Home() {
   return (
     <div>
       <HomezNavbar />
-      <h1 className="homeTitle">Welcome to HOMEZ: Responsible Ride-Sharing</h1>
-      <Link to="/ride">
-        <p>Need a ride? Sign up as a passenger</p>
-      </Link>
-      <Link to="/login">
-        <p>Already have an account? Sign in</p>
-      </Link>
-      <img className="laSkyline" src={laSkyline} alt="Los Angeles Skyline" />
+      <div style={{ position: "relative" }}>
+        <img className="laSkyline" src={laSkyline} alt="Los Angeles Skyline" style={{ width: "100%" }}/>
+        <Container fluid className="text-center" style={{ position: "absolute", top: "0", width: "100%", zIndex: 1 }}>
+          <Row>
+            <Col>
+              <h1 className="homeTitle" style={{ color: "white", textAlign: "center", fontSize: "40px", marginTop: "50px", marginBottom: "50px", fontWeight: "bolder" }}>Welcome to HOMEZ: Responsible Ride-Sharing</h1>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Card style={{ position: "absolute", bottom: "20px", left: "20px", zIndex: 2, backgroundColor: "rgba(255, 255, 255, 0.8)" }}>
+  <Card.Body>
+    <Link to="/ride" className="text-dark" style={{ textDecoration: 'none', fontSize: '20px' }}>
+      <Card.Text>Need a ride? Sign up as a passenger</Card.Text>
+    </Link>
+  </Card.Body>
+</Card>
+<Card style={{ position: "absolute", bottom: "20px", right: "20px", zIndex: 2, backgroundColor: "rgba(255, 255, 255, 0.8)" }}>
+  <Card.Body>
+    <Link to="/login" className="text-dark" style={{ textDecoration: 'none', fontSize: '20px' }}>
+      <Card.Text>Already have an account? Sign in</Card.Text>
+    </Link>
+  </Card.Body>
+</Card>
+
       <section className="blazeIt">
         <div className="blazeItText">
           <p>Earn $420.69 in Los Angeles every day</p>
@@ -25,20 +43,20 @@ function Home() {
         </div>
       </section>
       <div className="imageTextContainer">
-          <img className="behindWheel" src={behindWheel} alt="Behind the Wheel" />
-          <p className="rideParagraph">
-            Partied a little too hard and need a ride home for both you and your
-            car? Don't worry, we've got you covered. At HOMEZ, we understand that
-            a great night out sometimes means you're not in the best condition to
-            drive home. That's why we're here. With our easy to navigate website,
-            you can book a ride with your HOMEZ. We'll send two drivers - one to drive you
-            home in your own car, and another to pick up your driver. No more
-            stressing about leaving your car overnight or bothering friends for a
-            ride back to it the next day. With HOMEZ, you can enjoy your night to
-            the fullest and still make it home safe and sound. We've got your back
-            - and your car's too.
-          </p>
-        </div>
+        <img className="behindWheel" src={behindWheel} alt="Behind the Wheel" />
+        <p className="rideParagraph">
+          Partied a little too hard and need a ride home for both you and your
+          car? Don't worry, we've got you covered. At HOMEZ, we understand that
+          a great night out sometimes means you're not in the best condition to
+          drive home. That's why we're here. With our easy to navigate website,
+          you can book a ride with your HOMEZ. We'll send two drivers - one to drive you
+          home in your own car, and another to pick up your driver. No more
+          stressing about leaving your car overnight or bothering friends for a
+          ride back to it the next day. With HOMEZ, you can enjoy your night to
+          the fullest and still make it home safe and sound. We've got your back
+          - and your car's too.
+        </p>
+      </div>
     </div>
   );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button, Container, Col } from 'react-bootstrap';
 import HomezNavbar from '../components/Navbar';
 
 const LoginHomez = () => {
@@ -7,22 +7,26 @@ const LoginHomez = () => {
         <>
             <HomezNavbar />
 
-            <Container>
+            <Container className="d-flex justify-content-center">
+                <Col md={6}>
+                    <h1 className='text-center' style={{ marginTop: '50px' }}>Welcome Back to HOMEZ</h1>
+                    <Form style={{ marginBottom: '50px' }}>
+                        <Form.Group controlId="formGroupUsername" style={{ marginBottom: '20px' }}>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="username" placeholder="Enter your username" required />
+                            <Form.Control.Feedback type="invalid">Username is required!</Form.Control.Feedback>
+                        </Form.Group>
 
-                <h1 className='loginTitle'>Welcome Back to HOMEZ</h1>
-
-                <Form>
-                    <Form.Group className="mb-3" controlId="formGroupUsername">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="username" placeholder="Enter your username" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formGroupPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter your password" />
-                    </Form.Group>
-                    <Button className="LoginBtn" variant="outline-success">Welcome Back Homez!</Button>
-                </Form>
+                        <Form.Group controlId="formGroupPassword" style={{ marginBottom: '20px' }}>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter your password" required />
+                            <Form.Control.Feedback type="invalid">Password is required!</Form.Control.Feedback>
+                        </Form.Group>
+                        <div className="d-flex justify-content-center" style={{ marginBottom: '50px' }}>
+                            <Button type="submit" className="LoginBtn" variant="outline-success">Welcome Back Homez!</Button>
+                        </div>
+                    </Form>
+                </Col>
             </Container>
         </>
     );
