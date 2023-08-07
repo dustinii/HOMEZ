697186ff17368fb8f9965ae41d430e7965823b6b
femke77 <megan.meyers.388@gmail.com>
2023-08-07 14:28:56 -0700

    footer at bottom

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <>
-          <footer className="footer">
+          <footer className="footer fixed-bottom">
       <p>
         &copy; 2023 HOMEZ. All rights reserved.
       </p>
